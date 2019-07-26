c9b95d83708e3f3f30f9cd7b31ccd7ef18a70409
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-07-26 08:41:29 +0200

    BFS Day 1

--- AI8Puzzle/src/ai8puzzle/AI8Puzzle.java
+++ AI8Puzzle/src/ai8puzzle/AI8Puzzle.java
@@ -16,6 +16,7 @@ public class AI8Puzzle {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("BFS Day 1");
     }
     
 }
