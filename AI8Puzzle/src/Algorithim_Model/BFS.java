7c80cbc9b194da0112e733ffba4b13bb90ed5175
aghapygad336 <46167070+aghapygad336@users.noreply.github.com>
2019-07-27 11:12:11 +0200

    BFS Day 2  Green Team \M/

--- AI8Puzzle/src/Algorithim_Model/BFS.java
+++ AI8Puzzle/src/Algorithim_Model/BFS.java
@@ -13,4 +13,94 @@ import java.util.Stack;
  */
 public class BFS {
     
+ 
+    
+    public static void search(int[] board) {
+        Node root = new Node(board);
+        Queue<Node> queue = new LinkedList<Node>();
+
+        queue.add(root);
+
+        performSearch(queue);
+    
+    /*
+     * Helper method to check to see if a SearchNode has already been evaluated.
+     * Returns true if it has, false if it hasn't.
+     */
+    private static boolean checkRepeats(Node n) {
+        boolean retValue = false;
+        Node checkNode = n;
+
+        // While n's parent isn't null, check to see if it's equal to the node
+        // we're looking for.
+        while (n.getParent() != null && !retValue) {
+            if (n.getParent().getState().equals(checkNode.getState())) {
+                retValue = true;
+            }
+            n = n.getParent();
+        }
+
+        return retValue;
+    }
+
+    /**
+     * Performs a BFSearch using q as the search space
+     *
+     * @param q - A SearchNode queue to be populated and searched
+     */
+    public static void performSearch(Queue<Node> q) {
+        int searchCount = 1; // counter for number of iterations
+
+        while (!q.isEmpty()) // while the queue is not empty
+        {
+            Node tempNode = q.poll();
+
+            if (!tempNode.isGoal()) // if tempNode is not the goal
+                // state
+            {
+                int[] tempSuccessors = tempNode.getState();
+                for (int i = 0; i < tempSuccessors.length; i++) {
+                    int [] qq = null;
+                    // second parameter here adds the cost of the new node to
+                    // the current cost total in the SearchNode
+                    Node newNode = new Node(new EightPuzzleState(qq));
+                    
+                    if (!checkRepeats(newNode)) {
+                        q.add(newNode);
+                    }
+                }
+                searchCount++;
+            } else
+                // The goal state has been found. Print the path it took to get to
+                // it.
+            {
+                // Use a stack to track the path from the starting state to the
+                // goal state
+                Stack<Node> solutionPath = new Stack<Node>();
+                solutionPath.push(tempNode);
+                tempNode = tempNode.getParent();
+                
+                while (tempNode.getParent() != null) {
+                    solutionPath.push(tempNode);
+                    tempNode = tempNode.getParent();
+                }
+                solutionPath.push(tempNode);
+                
+                // The size of the stack before looping through and emptying it.
+                int loopSize = solutionPath.size();
+                
+                for (int i = 0; i < loopSize; i++) {
+                    tempNode = solutionPath.pop();
+                    tempNode.getState();
+                    System.out.println();
+                    System.out.println();
+                }
+                
+                System.exit(0);
+            }
+        }
+
+        // This should never happen with our current puzzles.
+        System.out.println("Error! No solution found!");
+    }
 }
